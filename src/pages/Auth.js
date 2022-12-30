c57a025b2bbec519ce4866917222a4a0189e2221
khw970421 <khw970421@kakao.com>
2022-12-30 15:04:19 +0900

    chore : 상태값 하위로 처리

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import { SIGN_UP } from "../utils/constants";
+
+import AuthForm from "../components/Auth/AuthForm";
 
 function Auth() {
-  const [signState, setSignState] = useState(SIGN_UP);
 
-  return <div>Auth</div>;
+  return (
+      <AuthForm />
+  );
 }
 
 export default Auth;
