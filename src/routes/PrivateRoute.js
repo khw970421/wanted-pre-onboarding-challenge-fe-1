1443b45376d93e2e11ce2e432f59c0c6bf46b313
khw970421 <khw970421@kakao.com>
2022-12-30 23:14:35 +0900

    feat : 토큰 유효성 검사
    토큰이 유효하지 않다면 사용자에게 알림

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -3,6 +3,7 @@ import { getLocalStorageToken } from "../utils/local-storage-fn";
 
 const PrivateRoute = ({ children }) => {
   const isLogined = getLocalStorageToken();
+  if (!isLogined) alert("유효한 토큰이 아닙니다.");
   return isLogined ? children : <Navigate to="/auth" />;
 };
 
